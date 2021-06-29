f6e96e04f5478c83b03607c0f44bfdf30d9363ac
Alfiyazabir05 <84541506+Alfiyazabir05@users.noreply.github.com>
2021-06-29 20:15:35 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Bootcamp on Javascript and React.js</h1>
+      <p>
+        This Bootcamp will be taken by Shaurya Sinha Sir 7 days from 22nd Jun at
+        6:00 Pm
+      </p>
+    </div>
+  );
+}
+export default Note;
